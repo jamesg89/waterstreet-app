3f6f4496d02e0930164a308ed89bf98d67f28ab4
jamesg89 <jamesgoodwinrealty@gmail.com>
2026-02-07 21:16:36 -0800

    Page Config from AI

--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,24 @@ import type { SonicJSConfig } from '@sonicjs-cms/core'
 // Import collection configurations
 import pagesCollection from './collections/pages.collection'
 import servicesCollection from './collections/services.collection'
-import pricingCollection from './collections/pricing.collection'
+import statsCollection from './collections/stats.collection'
+import processStepsCollection from './collections/process_steps.collection'
+import faqsCollection from './collections/faqs.collection'
+import contactMethodsCollection from './collections/contact_methods.collection'
+import siteSettingsCollection from './collections/site_settings.collection'
+import contactSubmissionsCollection from './collections/contact_submissions.collection'
 
 // Register collections BEFORE creating the app
 // This ensures they are synced to the database on startup
 registerCollections([
   pagesCollection,
   servicesCollection,
-  pricingCollection,
+  statsCollection,
+  processStepsCollection,
+  faqsCollection,
+  contactMethodsCollection,
+  siteSettingsCollection,
+  contactSubmissionsCollection,
 ])
 
 // Application configuration
